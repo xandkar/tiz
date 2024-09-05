b2984b282429aa23491d2b605fb9d065e40e156a
Siraaj Khandkar <siraaj@khandkar.net>
2024-09-05 09:46:27 -0400

    Refactor

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,12 @@ use chrono::{Timelike, Utc};
 use chrono_tz::Tz;
 
 fn main() -> anyhow::Result<()> {
+    let zones = args_parse()?;
+    println!("{}", table(&zones[..], tz_local()));
+    Ok(())
+}
+
+fn args_parse() -> anyhow::Result<Vec<Tz>> {
     let mut args = std::env::args();
     let exec = args.next().unwrap_or_else(|| unreachable!());
     let args: Vec<String> = args.collect();
@@ -18,34 +24,42 @@ fn main() -> anyhow::Result<()> {
             .map_err(|_| anyhow!("Invalid time zone: {:?}", zone))?;
         zones.push(zone);
     }
-    let tz_local: Option<Tz> = std::env::var("TZ").ok().and_then(|tz| tz.parse().ok());
-    let hour_in_zone: HashMap<Tz, u32> = zones
+    Ok(zones)
+}
+
+fn tz_local() -> Option<Tz> {
+    std::env::var("TZ").ok().and_then(|tz| tz.parse().ok())
+}
+
+fn tz_hours_ordered(zones: &[Tz]) -> Vec<(Tz, u32)> {
+    let tz_hours: HashMap<Tz, u32> = zones
         .into_iter()
-        .map(|tz| (tz, Utc::now().with_timezone(&tz).hour()))
+        .map(|tz| (*tz, Utc::now().with_timezone(tz).hour()))
         .collect();
-    let mut hour_in_zone: Vec<(Tz, u32)> = hour_in_zone.into_iter().collect();
-    hour_in_zone.sort_by_key(|(_, h)| *h);
-    let hours: Vec<u32> = (0..24).collect();
+    let mut tz_hours_ordered: Vec<(Tz, u32)> = tz_hours.into_iter().collect();
+    tz_hours_ordered.sort_by_key(|(_, hour)| *hour);
+    tz_hours_ordered
+}
+
+fn table(zones: &[Tz], tz_local: Option<Tz>) -> comfy_table::Table {
     let mut table = comfy_table::Table::new();
     table.load_preset(comfy_table::presets::NOTHING); // No borders or dividers.
-    for (tz, tz_h) in hour_in_zone {
-        let mut row: Vec<String> = hours
-            .iter()
-            .map(|h| {
-                let hour = console::style(h);
-                if *h == tz_h {
-                    match tz_local {
-                        Some(tz_local) if tz == tz_local => hour.green().bold().to_string(),
-                        _ => hour.white().bold().to_string(),
+    for (tz, tz_hour) in tz_hours_ordered(zones) {
+        let mut row: Vec<String> = (0..24)
+            .map(|hour| {
+                let hour_style = console::style(hour);
+                match (hour == tz_hour, tz_local) {
+                    (true, Some(tz_local)) if tz == tz_local => {
+                        hour_style.green().bold()
                     }
-                } else {
-                    hour.white().dim().to_string()
+                    (true, _) => hour_style.white().bold(),
+                    _ => hour_style.white().dim(),
                 }
+                .to_string()
             })
             .collect();
         row.insert(0, tz.to_string());
         table.add_row(row);
     }
-    println!("{}", table);
-    Ok(())
+    table
 }
